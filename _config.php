7aca1d8e6372ae59d485ee50253d76c6425851f6
Ketan Shah <ketan.shah@gmail.com>
2014-01-18 10:08:59 +0530

    initial working commit

---
+++ _config.php
@@ -0,0 +1,13 @@
+<?php 
+	require 'vendor/autoload.php';
+
+	use Mrk\Mailgun\Mailer as MrkMailgunMailer;
+
+	define('MAILGUN_KEY', 'key-01zghfgcxo4ta4t4j7h8g9pyi4xfgmn0');
+	define('MAILGUN_DOMAIN', 'sandbox8297.mailgun.org');
+	define('MAILGUN_FROM', 'ketan.shah@gmail.com');
+
+
+	Email::set_mailer(new MrkMailgunMailer);
+
+ ?>
---
+++ code/mrk/mailgun/Mailer.php
@@ -0,0 +1,75 @@
+<?php namespace Mrk\Mailgun;
+
+use Mailgun\Mailgun;
+	
+class Mailer extends \Mailer{
+	private $mailgun;
+
+
+
+	/**
+	 * [__construct description]
+	 */
+	function __construct() {
+		$this->mailgun = new Mailgun(MAILGUN_KEY);
+	}
+
+
+
+	/**
+	 * [sendPlain description]
+	 * @param  [type]  $to            [description]
+	 * @param  [type]  $from          [description]
+	 * @param  [type]  $subject       [description]
+	 * @param  [type]  $plainContent  [description]
+	 * @param  boolean $attachedFiles [description]
+	 * @param  boolean $customheaders [description]
+	 * @return [type]                 [description]
+	 */
+	public function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = array(), $customheaders = false) {
+		$message = array(
+						'from'    => MAILGUN_FROM, 
+                        'to'      => $to, 
+                        'subject' => $subject, 
+                        'text'    => $plainContent
+                       );
+
+		return $this->mailgun
+					->sendMessage(MAILGUN_DOMAIN, $message, $attachedFiles);
+
+	}
+
+
+	/**
+	 * [sendHTML description]
+	 * @param  [type]  $to            [description]
+	 * @param  [type]  $from          [description]
+	 * @param  [type]  $subject       [description]
+	 * @param  [type]  $htmlContent   [description]
+	 * @param  boolean $attachedFiles [description]
+	 * @param  boolean $customheaders [description]
+	 * @param  boolean $plainContent  [description]
+	 * @return [type]                 [description]
+	 */
+	public function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = array(), $customheaders = false,
+			$plainContent = false) {
+
+		$message = array(
+						'from'    => MAILGUN_FROM, 
+                        'to'      => $to, 
+                        'subject' => $subject, 
+                        'html'    => $htmlContent
+                );
+
+		if($plainContent)
+			$message['text'] = $plainContent;
+
+
+		return $this->mailgun
+					->sendMessage(MAILGUN_DOMAIN, $message, $attachedFiles);
+
+
+	}
+}
+
+ ?>
